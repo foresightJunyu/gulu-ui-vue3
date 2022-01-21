76a31d0f9b40e87e4f0a5bbb8737db62e43fe5cb
w-lujunyu <w-lujunyu@rootcloud.com>
2022-01-21 21:59:45 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/Hell111oWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
